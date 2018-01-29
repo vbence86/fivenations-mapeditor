a5403b58456fe81c5146d04c566a33246a28ddd1
Bence Varga <vbence86@gmail.com>
2018-01-29 09:40:00 +0100

    add source folder

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+/* global window */
+import '../../lib/EZGUI.js';
+import '../../lib/fivenations.0.42.0.js';
+
+const fivenations = window.fivenations;
+
+console.log(`Five Nations Map Editor - using Five Nations v${fivenations.version}`);
+
