3f8698a1edbe06540c1246a2b78d05dc3dd6a36f
Bence Varga <vbence86@gmail.com>
2019-10-03 09:49:25 +0200

    use https to clone fivenations into dependencies

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 
 shell.cd('node_modules');
 shell.rm('-rf', 'fivenations');
-shell.exec('git clone --progress --depth 1 -b mapeditor git@github.com:vbence86/fivenations.git');
+shell.exec('git clone --progress --depth 1 -b mapeditor https://github.com/vbence86/fivenations.git');
 shell.cd('fivenations');
 shell.exec('npm install');
 shell.exec('npm run build-library');
